16584d17c7336ac8625f36a778f6d70d76733407
voyager <cdspacenoob@gmail.com>
2023-01-10 17:29:41 +0900

    props 연습문제

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpensItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -29,10 +30,13 @@ function App() {
       {/* p는 단락을 의미 */}
       <p>This is also visible!</p>
       {/* 대문자로 시작하는 요소는 커스텀된 요소임을 표시한다 */}
-      <ExpensItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      {/* <ExpensItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpensItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpensItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpensItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <ExpensItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} /> */}
+
+      {/* props에 data라는 이름으로 expenses를 보내겠다. */}
+      <Expenses data={expenses} />
     </div>
   );
 
